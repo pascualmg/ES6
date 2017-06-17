f0f71e594818d59bc3f70eed594c03306d53273f
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-06-17 17:22:19 +0200

    array con funciones y una funcion que recorre array llamando a todas

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
-
-//yo soy window. 
+//yo soy window.
 //Esto se va a ejecutar en cascada cada vez que se hace ctrl+r de la pag.
 
 let  saludo = "hola caracola, ";
 
-function dialgoAlosPavos() {
+function diSaludo()
+{
+    console.log(saludo);
+}
+
+function dialgoAlosPavos()
+{
     console.log("PAAAAAVO VAAAVOVOVOVOVO");
 }
-const PERSONA = { nombre: "pascual", edad: "22", hablar: window.dialgoAlosPavos };
-PERSONA.nombre = "antonia";
-for (let i=0;i<10;i++)
-    console.log(PERSONA);
+function diAdios()
+{
+    console.log("ADIOS QUE TE TEN");
+}
+
+let funcs = [];
+funcs.push(window.diSaludo);
+funcs.push(window.dialgoAlosPavos);
+funcs.push(window.diAdios);
 
 
+function diToloquesepas()
+{
+    for (let func of funcs) {
+        if (typeof func === "function") {
+            func();
+        }
+    }
+}
+
+function diToloquesepasForeach()
+{
+    funcs.forEach(function (f) {
+        f();
+    })
+}
 
 
+const PERSONA = {
+    nombre: "pascual",
+    edad: "22",
+    hablar: window.diToloquesepasForeach
+
+};
+
+PERSONA.hablar();
+PERSONA.hablar = window.diToloquesepas;
+PERSONA.hablar();
+
+
+let nombre = "Roberto";
+
+if ( nombre === "Roberto" ) {
+    let nombre = "Juan Carlos";
+    console.log(nombre);
+}
+
+console.log(nombre);
