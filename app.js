7a32ce152affc32b112714f2a2c13e0968240d35
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-06-18 03:38:03 +0200

    funciones instanciadas con new vs no new

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-//4.17 OPERADOR SPREAD.
+//
 log();
 
+function Persona(name) {
 
-log(hola);
+    if (typeof new.target === 'undefined')
+    {
+
+    }
+    this.nombre = name;
+}
+
+var persona = new Persona("alfonsa");
+var nopersona = Persona("alfonso");
+
+log("algo");
+log(persona);
+log(nopersona);
